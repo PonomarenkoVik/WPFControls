616121c290130397a5c62519ec02dfa391e0e53e
dev <pvikmboxhome@gmail.com>
2019-08-08 23:40:03 +0300

    PagingPointControl

---
+++ PagingControl/PagingControl/PagingPointsControl.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PagingControl
+{
+    /// <summary>
+    /// Interaction logic for PagingPointsControl.xaml
+    /// </summary>
+    public partial class PagingPointsControl : UserControl
+    {
+        public PagingPointsControl()
+        {
+            InitializeComponent();
+        }
+
+        public double PointDiameter { get; set; } = 6;
+        public double MinPointDiameter { get; set; } = 3;
+
+        private int _currentPoint;
+        private int _from;
+        public void SetPointsNumber(int currentPoint, int from)
+        {
+            _currentPoint = currentPoint;
+            _from = from;
+            DrawPoints();
+        }
+
+
+
+        private void DrawPoints()
+        {
+            double d;
+            if (_from * (PointDiameter + 0.5 * PointDiameter) > this.Width)
+	        {
+                d = (this.Width/_from)/1.5;
+                d = d < MinPointDiameter ? MinPointDiameter : d;
+            }
+            else
+                d = PointDiameter;
+
+            double x = (this.Width - (_from * d * 1.5) - 2.2 * d)/2;
+            Canv.Children.Clear();
+            for (int i = 0; i < _from; i++)
+            {
+                x += d * 1.5;
+                Ellipse ell = new Ellipse() { Width = d, Height = d, Stroke = Foreground, StrokeThickness = 1};
+                if (i + 1 == _currentPoint)
+                    ell.Fill = Foreground;
+
+                ell.SetValue(Canvas.LeftProperty, x);
+                ell.SetValue(Canvas.TopProperty, Height / 2 - d / 2);
+                Canv.Children.Add(ell);
+            }
+
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+        }
+
+    }
+}
